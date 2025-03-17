e3c68278841b3956b50cdadf1065fdae9b5102e4
abi2501 <abiemailid@gmail.com>
2025-03-17 17:01:43 +0530

    Prod changes

--- src/db.js
+++ src/db.js
@@ -7,11 +7,18 @@ const connection = await mysql.createConnection({
   // user: "root",
   // password: "root",
   // database: "tododb",
-  host: "bffrulrlzht15svbzxgq-mysql.services.clever-cloud.com",
-  user: "uhimfojchgauenwc",
-  password: "qkQyG1oPizLcJ7pg3xao",
-  database: "bffrulrlzht15svbzxgq",
+  // host: "bffrulrlzht15svbzxgq-mysql.services.clever-cloud.com",
+  // user: "uhimfojchgauenwc",
+  // password: "qkQyG1oPizLcJ7pg3xao",
+  // database: "bffrulrlzht15svbzxgq",
+  // port: 3306,
+
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   port: 3306,
+  waitForConnections: true,
 });
 
 const query = async (text, params) => {
